bff9b80dbda42b30fb1085f5df265d07d690b441
Ben Bates <ben.bates@runkeeper.com>
2012-12-06 15:40:39 -0500

    Improving Player Listing

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -80,4 +80,5 @@ class PlayersController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
 end
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,15 @@
 module PlayersHelper
+  
+  def points_against(player)
+    score = 0
+    player.games.each do |game|
+      game.results.each do |result|
+        if result.player_id != player.id 
+          score = score +result.score
+        end
+      end
+    end
+    score
+  end
+  
 end

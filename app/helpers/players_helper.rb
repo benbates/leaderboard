fc17fc74b35e94dacfd750ff9ec28a9679a2301d
Ben Bates <ben.bates@runkeeper.com>
2012-12-06 21:08:04 -0500

    Fixed 'points against' (need to optimize)

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,15 +1,3 @@
 module PlayersHelper
   
-  def points_against(player)
-    score = 0
-    player.games.each do |game|
-      game.results.each do |result|
-        if result.player_id != player.id 
-          score = score +result.score
-        end
-      end
-    end
-    score
-  end
-  
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,19 @@
 class Player < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :points_against
   
 	has_many :games, :through => :results
   has_many :results
+  
+  def points_against
+    score = 0
+    self.games.each do |game|
+      game.results.each do |result|
+        if result.player_id != self.id 
+          score = score +result.score
+        end
+      end
+    end
+    score
+  end
+  
 end

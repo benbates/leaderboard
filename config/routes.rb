c7a8123f0447edca23deaa099f3599022d8d1037
Ben Bates <ben.bates@runkeeper.com>
2013-01-12 16:19:42 -0500

    Root path to leaderboard - fix root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Leaderboard::Application.routes.draw do
+  root to: 'players#index'
+  
   resources :game_types
 
   resources :results
@@ -56,7 +58,7 @@ Leaderboard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'players'
+   
 
   # See how all your routes lay out with "rake routes"
 

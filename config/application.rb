aede3126a220c6a7ffa1051f9f5f3794260a57a8
Ben Bates <ben.bates@runkeeper.com>
2013-02-12 08:27:50 -0500

    jQuery not defined

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,6 @@ module Leaderboard
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.precompile += %w('jquery.js')
+    config.assets.precompile += ['jquery.js']
   end
 end

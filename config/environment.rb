53a65b019af0cdd67274feb7538912f0123c5095
Ben Bates <ben.bates@runkeeper.com>
2013-03-13 22:44:20 -0400

    environment.rb changes messing up on heroku, removed

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Leaderboard::Application.initialize!
-
-module Callbacks
-  def callbacks_for_with_uniq(method)
-    callbacks_for_without_uniq(method).uniq
-  end
-
-  alias_method_chain :callbacks_for, :uniq
-end
